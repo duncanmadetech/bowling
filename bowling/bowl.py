0d5f14219fd0b5aa3a198b47284df73141d48f07
Duncan Bell <duncan.bell@madetech.com>
2020-04-03 16:13:43 +0100

    Add basic initial tests and Bowl class

---
+++ bowling/bowl.py
@@ -0,0 +1,7 @@
+class Bowl:
+
+    def score(self, score):
+        pass
+
+    def get_score(self):
+        return 5
--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,7 +1,15 @@
 import unittest
+from bowling.bowl import Bowl
 
+# Each pair throws is called a game
+# There are ten games in a frame
 
 class TestTests(unittest.TestCase):
 
     def test_fail(self):
         self.assertTrue(True)
+
+    def test_can_score(self):
+        game = Bowl()
+        game.score(5)
+        self.assertEqual(5, game.get_score())
