b7c597f467582d5e4afb48846a04da59468673f5
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-17 14:35:24 +0100

    refactor to score a frame at a time

--- bowling/bowl.py
+++ bowling/bowl.py
@@ -3,8 +3,8 @@ class Bowl:
     def __init__(self):
         self.current_score = 0
 
-    def score(self, score):
-        self.current_score += score
+    def score(self, bowl1, bowl2):
+        self.current_score = self.current_score + bowl1 + bowl2
 
     def get_score(self):
         return self.current_score
--- tests/test_tests.py
+++ tests/test_tests.py
@@ -9,16 +9,9 @@ import random
 
 class TestTests(unittest.TestCase):
 
-    def test_can_score_a_throw(self):
-        score = random.randint(1, 9)
-        game = Bowl()
-        game.score(score)
-        self.assertEqual(score, game.get_score())
-
     def test_can_score_a_frame(self):
         game = Bowl()
-        game.score(5)
-        game.score(3)
+        game.score(5, 3)
         self.assertEqual(8, game.get_score())
 
     def test_initial_score_is_zero(self):
