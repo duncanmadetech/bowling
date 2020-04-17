27fd1a8e4a1931ad6bf3ff351aa8ad2ab8e62ac6
Anthony Robinson <anthony.robinson@madetech.com>
2020-04-17 14:27:42 +0100

    clarify terms

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -2,22 +2,24 @@ import unittest
 from bowling.bowl import Bowl
 import random
 
-# Each two attempt is called a go
-# There are ten goes in a frame
+# https://learn.madetech.com/katas/bowling/
+# Each bowl is called a throw
+# Each two attempt is called a frame
+# There are ten frames in a line
 
 class TestTests(unittest.TestCase):
 
-    def test_can_score(self):
+    def test_can_score_a_throw(self):
         score = random.randint(1, 9)
         game = Bowl()
         game.score(score)
         self.assertEqual(score, game.get_score())
 
-    def test_can_score_from_two_goes(self):
+    def test_can_score_a_frame(self):
         game = Bowl()
         game.score(5)
-        game.score(7)
-        self.assertEqual(12, game.get_score())
+        game.score(3)
+        self.assertEqual(8, game.get_score())
 
     def test_initial_score_is_zero(self):
         self.assertEqual(0, Bowl().get_score())
